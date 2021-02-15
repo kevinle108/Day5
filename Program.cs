67ea76cbffbcfe5b14cec7c3dc2e7c0425b15e49
Kevin Le <kevinle108@gmail.com>
2021-02-15 09:57:23 -0500

    add looping for continuous input

--- Program.cs
+++ Program.cs
@@ -9,38 +9,44 @@ namespace Day5
         static void Main(string[] args)
         {
             int num;
-            Console.Write("Enter a positive number (or 0 to quit): ");
-            if (int.TryParse(Console.ReadLine(), out num) && num >= 0)
+            while (true)
             {
-                
-                if (num == 0) return;
-                string output = $"{num} = ";
-                if (num == 1)
-                {
-                    Console.WriteLine(output += '1');
-                    return;
-                }
-                List<int> primesList = new List<int>();
-                int factor = 2;
-                while (num != 1)
-                {
-                    if (num % factor == 0)
-                    {
-                        primesList.Add(factor);
-                        num = num / factor;
-                    }
-                    else
-                    {
-                        factor++;
-                    }
-                }
-                output += String.Join(" x ", primesList.ToArray());
-                Console.WriteLine(output);
+                Console.Write("Enter a positive number (or 0 to quit): ");
 
-            } else
-            {
-                Console.WriteLine("Invalid input!");
+                if (int.TryParse(Console.ReadLine(), out num) && num >= 0)
+                {
+
+                    if (num == 0) break;
+                    string output = $"{num} = ";
+                    if (num == 1)
+                    {
+                        Console.WriteLine(output += '1');
+                        return;
+                    }
+                    List<int> primesList = new List<int>();
+                    int factor = 2;
+                    while (num != 1)
+                    {
+                        if (num % factor == 0)
+                        {
+                            primesList.Add(factor);
+                            num = num / factor;
+                        }
+                        else
+                        {
+                            factor++;
+                        }
+                    }
+                    output += String.Join(" x ", primesList.ToArray());
+                    Console.WriteLine(output);
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
             }
+            Console.WriteLine("Goodbye!");
 
 
         }
